2e36c08589a1be35eba008b592a7b9bea8662893
eunji8784 <eunjikim8784@naver.com>
2021-11-27 00:04:51 +0900

    [fix]: videoDetailGET.js 수정

--- functions/api/routes/video/videoDetailGET.js
+++ functions/api/routes/video/videoDetailGET.js
@@ -1,12 +1,12 @@
-const functions = require("firebase-functions");
-const util = require("../../../lib/util");
-const statusCode = require("../../../constants/statusCode");
-const responseMessage = require("../../../constants/responseMessage");
-const db = require("../../../db/db");
-const { videoDB, commentDB } = require("../../../db");
+const functions = require('firebase-functions');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { videoDB, commentDB } = require('../../../db');
 
 module.exports = async (req, res) => {
-  console.log("동영상 Detail 조회 API 호출");
+  console.log('동영상 Detail 조회 API 호출');
   const { videoId } = req.params;
   let client;
 
@@ -14,17 +14,49 @@ module.exports = async (req, res) => {
     client = await db.connect(req);
 
     let video = await videoDB.getOneVideo(client, videoId);
-
     let comments = await commentDB.getCommentsByVideoId(client, videoId);
 
-    res
-      .status(statusCode.OK)
-      .send(
-        util.success(statusCode.OK, responseMessage.READ_ONE_VIDEOS_SUCCESS, {
-          video,
-          comments,
-        })
-      );
+    const tags = video.tags.split('#');
+    video = {
+      videoID: video.videoId,
+      title: video.title,
+      tags,
+      viewCount: video.viewCount,
+      createdAt: video.createdAt,
+      author: {
+        userID: video.userId,
+        nickname: video.nickname,
+        profileImage: video.profileImage,
+      },
+      description: video.description,
+      runtime: video.runtime,
+      thumbnail: video.thumbnail,
+      isHot: video.isHot,
+      isLike: video.isLike,
+      isDislike: video.isDislike,
+      likeCount: video.likeCount,
+      dislikeCount: video.dislikeCount,
+    };
+
+    comments = comments.map((comment) => {
+      return {
+        commentID: comment.commentId,
+        commenter: {
+          userID: comment.userId,
+          nickname: comment.nickname,
+          profileImage: comment.profileImage
+        },
+        createdAt: comment.createdAt,
+        content: comment.content
+      }
+    });
+  
+    res.status(statusCode.OK).send(
+      util.success(statusCode.OK, responseMessage.READ_ONE_VIDEOS_SUCCESS, {
+        video,
+        comments,
+      })
+    );
   } catch (error) {
     functions.logger.error(
       `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
