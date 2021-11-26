9ac2d780a993e201d5f4e928a7baf67b21be866e
eunji8784 <eunjikim8784@naver.com>
2021-11-27 00:04:05 +0900

    [fix]: commentPOST.js 수정

--- functions/api/routes/video/commentPOST.js
+++ functions/api/routes/video/commentPOST.js
@@ -6,23 +6,57 @@ const db = require('../../../db/db');
 const { commentDB } = require('../../../db');
 
 module.exports = async (req, res) => {
-    console.log("댓글 등록 API 호출");
-    const { videoId }  = req.params;
-    const { userId, content } = req.body;
+  console.log('댓글 등록 API 호출');
+  const { videoId } = req.params;
+  const { userId, content } = req.body;
 
-    let client;
-  
-    try {
-      client = await db.connect(req);
-      const data = await commentDB.postVideoComment(client, videoId, userId, content);
-  
-      res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.ADD_COMMENT_SUCCESS, data));
-    } catch (error) {
-      functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
-      console.log(error);
-  
-      res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-    } finally {
-      client.release();
-    }
-  };
+  let client;
+
+  try {
+    client = await db.connect(req);
+    const postedComment = await commentDB.postVideoComment(
+      client,
+      videoId,
+      userId,
+      content
+    );
+
+    const comment = await commentDB.getCommentByCommentId(
+      client,
+      postedComment.commentId
+    );
+
+    const data = {
+      commentID: comment.commentID,
+      commenter: {
+        userID: comment.userID,
+        nickname: comment.nickname,
+        profileImage: comment.profileImage
+      },
+      content: comment.content,
+      createdAt: comment.createdAt
+    };
+    res
+      .status(statusCode.OK)
+      .send(
+        util.success(statusCode.OK, responseMessage.ADD_COMMENT_SUCCESS, data)
+      );
+  } catch (error) {
+    functions.logger.error(
+      `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+      `[CONTENT] ${error}`
+    );
+    console.log(error);
+
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(
+        util.fail(
+          statusCode.INTERNAL_SERVER_ERROR,
+          responseMessage.INTERNAL_SERVER_ERROR
+        )
+      );
+  } finally {
+    client.release();
+  }
+};
